3e37250df889850d06766d831cf76a98a9ca6d28
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-23 10:19:56 +0900

    docs: Client UI라는 용어를 Service Client로 변경

--- src/components/TelbbyServiceClient/index.tsx
+++ src/components/TelbbyServiceClient/index.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 import { serviceClientWrapper } from './style';
 
 const TelbbyServiceClient: FC = () => {
-  // @TODO 여기에서 Client UI를 구현해나갑니다.
+  // @TODO 여기에서 Service Client를 구현해나갑니다.
   return <div css={serviceClientWrapper} />;
 };
 
